95962ce4aecf56582b3cdb0a66f2983653f08985
Vicki <ymasakot@gmail.com>
2023-08-09 23:46:16 +0800

    init

---
+++ FacebookNewFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FacebookNewFeed
+//
+//  Created by Vicki Yang on 2023/8/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
