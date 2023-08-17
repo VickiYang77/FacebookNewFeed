77b27b6be1c9f670b39f431c0abc916f5f53512d
Vicki <ymasakot@gmail.com>
2023-08-17 23:38:11 +0800

    新增檔案

---
+++ FacebookNewFeed/ViewController+Extension.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController+Extension.swift
+//  FacebookNewFeed
+//
+//  Created by Vicki Yang on 2023/8/17.
+//
+
+import UIKit
+
+extension UIColor {
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+}
+
+extension UIView {
+    func addConstraintsWithFormat(format: String, views: UIView...) {
+        var viewDictionary = [String: UIView]()
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            viewDictionary[key] = view
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, metrics: nil, views: viewDictionary))
+    }
+}
